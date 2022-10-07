358e400fe7b3d5028dee82942f70ce17d6a1878a
Jwaegebaert <jwaegebaert@gmail.com>
2022-10-07 19:19:20 +0200

    Add readme

--- PRHatTip/index.ts
+++ PRHatTip/index.ts
@@ -1,17 +1,17 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import DiscordJS, { GatewayIntentBits, Partials, TextChannel } from 'discord.js';
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import DiscordJS, { GatewayIntentBits, TextChannel } from 'discord.js';
 import crypto from 'crypto';
-import axios from "axios";
+import axios from 'axios';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const sig = Buffer.from(req.headers['x-hub-signature-256'] || '', 'utf8');
-  const hmac = crypto.createHmac('sha256', process.env["HatTipSecret"]);
+  const hmac = crypto.createHmac('sha256', process.env['HatTipSecret']);
   const digest = Buffer.from('sha256=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
 
   if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
     context.res = {
       status: 401,
-      body: "Invalid key!"
+      body: 'Invalid key!'
     }
   }
 
@@ -22,14 +22,14 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const user = await axios.get(prInfo.user.url);
     const name = user.data.name || user.data.login;
     const defaultMessages = [
-      `${name}'s wonderful PR has just been merged. Thanks so much for your contribution! 🚀`,
-      `The CLI for M365 has just been upgraded with the help of ${name}. Thank you! 👏`,
+      `${name}'s wonderful PR has just been merged. Thank you so much for your contribution! 🚀`,
+      `CLI for Microsoft 365 has just been upgraded with the help of ${name}. Thank you! 👏`,
       `A hat tip to ${name} for their awesome contribution. 🎩`,
       `${name} has just contributed '${prInfo.title}'. Thank you! 👏`,
     ];
 
     const docPRMessages = [
-      `The pen is mightier than the sword, that's for sure! 📚 ${name} proved that with improved docs.`
+      `The pen is mightier than the sword, that's for sure! 📚 ${name} proved just that by improving the docs.`
     ];
 
     const bugPRMessages = [
@@ -55,11 +55,11 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     });
 
     client.on('ready', async () => {
-      const channel = (await client.channels.fetch('1022525781978660955')) as TextChannel;
+      const channel = (await client.channels.fetch(process.env['HatTipChannelId'])) as TextChannel;
       channel.send(message);
     });
 
-    client.login(process.env["ParkerToken"]);
+    client.login(process.env['ParkerToken']);
   }
 
   context.res = {
---
+++ README.md
@@ -0,0 +1,13 @@
+# Discord Parker Functions
+
+Discord Parker Functions are some basic Azure Functions developed within NodeJs. They are designed to feed our custom Discord app, `Discord Parker App`, with information about the PnP community. Currently, the main information the Parker App receives is all about the [CLI for Microsoft 365](https://github.com/pnp/cli-microsoft365). This information will be fed to a community bot that in turn will post notifications within the Discord server.
+
+To see him in action you can join the CLI for M365 community Discord server using the invite below.
+
+<br/>
+<p align="center">
+  <a href="https://discord.com/invite/7rfW4kg6B5">
+    <img src="https://img.shields.io/badge/Discord-invite/7rfW4kg6B5-7289da?style=for-the-badge"
+      alt="Discord" />
+  </a>
+</p>
