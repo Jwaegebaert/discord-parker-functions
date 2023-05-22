dc0cd586faf8a496c2cceb4783f17472155a4a2d
Jwaegebaert <38426621+Jwaegebaert@users.noreply.github.com>
2023-05-22 12:06:45 +0200

    Resolve tag char limit for threads

--- BlogPost/index.ts
+++ BlogPost/index.ts
@@ -2,6 +2,7 @@ import DiscordJS, { ForumChannel, GatewayIntentBits } from 'discord.js';
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import he from 'he';
 import { convert } from 'html-to-text';
+import { PnPBlogTags } from '../Data/Tags';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const client = new DiscordJS.Client({
@@ -12,13 +13,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
   client.on('ready', async () => {
     const channel = (await client.channels.fetch(process.env['BlogChannelId'])) as ForumChannel;
-
-    if (!channel.availableTags.some(x => x.name.toLocaleLowerCase() === req.body.category.toLocaleLowerCase())) {
-      await channel.setAvailableTags([
-        ...channel.availableTags,
-        { name: req.body.category }
-      ]);      
-    }
 
     await channel.threads.create({
       name: req.body.title, 
@@ -39,16 +33,32 @@ ${
 Read more: ${req.body.link}
         `,
       },
-      ...(channel.availableTags.some(x => x.name.toLocaleLowerCase() === req.body.category.toLocaleLowerCase()) 
-        && {appliedTags: [channel.availableTags.find(x => x.name.toLocaleLowerCase() === req.body.category.toLocaleLowerCase()).id]})
+      ...(isTagAvailable(channel, req.body.category)
+        && {appliedTags: [getTagId(channel, req.body.category)]})
     });
   });
 
   client.login(process.env['ParkerToken']);
-  
+
   context.res = {
     status: 204
   };
 };
+
+function isTagAvailable(channel: ForumChannel, categoryName: string): boolean {
+  return channel.availableTags.some(x => 
+    x.name.toLocaleLowerCase() === PnPBlogTags.find(y => 
+      y.name.toLocaleLowerCase() === categoryName.toLocaleLowerCase()
+    ).tagName.toLocaleLowerCase()
+  );
+}
+
+function getTagId(channel: ForumChannel, categoryName: string): string {
+  return channel.availableTags.find(x => 
+    x.name.toLocaleLowerCase() === PnPBlogTags.find(y => 
+      y.name.toLocaleLowerCase() === categoryName.toLocaleLowerCase()
+    ).tagName.toLocaleLowerCase()
+  ).id;
+}
 
 export default httpTrigger;
---
+++ Data/Tags.ts
@@ -0,0 +1,91 @@
+interface ITag {
+  name: string;
+  tagName: string;
+};
+
+export const PnPBlogTags: ITag[] = [
+  {
+    name: 'Adaptive Cards community call',
+    tagName: 'Adaptive Cards call'
+  },
+  {
+    name: 'CLI for Microsoft 365',
+    tagName: 'CLI for M365'
+  },
+  {
+    name: 'Community post',
+    tagName: 'Community post'
+  },
+  {
+    name: 'Microsoft 365 and power platform development community call',
+    tagName: 'Dev call'
+  },
+  {
+    name: 'Microsoft 365 developer podcast',
+    tagName: 'M365 dev podcast'
+  },
+  {
+    name: 'Microsoft 365 platform call',
+    tagName: 'M365 platform call'
+  },
+  {
+    name: 'Microsoft 365 platform community update',
+    tagName: 'M365 platform update'
+  },
+  {
+    name: 'Microsoft 365 power platform community update',
+    tagName: 'Power Platform'
+  },
+  {
+    name: 'Microsoft Graph community call',
+    tagName: 'Microsoft Graph call'
+  },
+  {
+    name: 'Microsoft identity platform community call',
+    tagName: 'Identity call'
+  },
+  {
+    name: 'Microsoft Teams community call',
+    tagName: 'Teams community call'
+  },
+  {
+    name: 'Microsoft Teams development',
+    tagName: 'Teams development'
+  },
+  {
+    name: 'Microsoft Viva and SPFx community call',
+    tagName: 'Viva & SPFx call'
+  },
+  {
+    name: 'Office add in developer community call',
+    tagName: 'Office add-in call'
+  },
+  {
+    name: 'Office add ins community call',
+    tagName: 'Office add-in call'
+  },
+  {
+    name: 'PnP core sdk',
+    tagName: 'PnP core sdk'
+  },
+  {
+    name: 'PnP PowerShell',
+    tagName: 'PnP PowerShell'
+  },
+  {
+    name: 'PnP weekly',
+    tagName: 'PnP weekly'
+  },
+  {
+    name: 'Power Apps community call',
+    tagName: 'Power Apps call'
+  },
+  {
+    name: 'SPFx',
+    tagName: 'SPFx'
+  },
+  {
+    name: 'Weekly agenda',
+    tagName: 'Weekly agenda'
+  }
+];
