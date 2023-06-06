78efb429812091b3ccb9343d0833924a56dd4fbc
Jwaegebaert <38426621+Jwaegebaert@users.noreply.github.com>
2023-06-06 20:36:53 +0200

    Added more HatTip messages + Resolve tags issue

--- Data/HatTipMessages.ts
+++ Data/HatTipMessages.ts
@@ -1,9 +1,9 @@
-interface IHatTip {
+export interface IHatTip {
   message: string;
   emojis: string[];
 };
 
-export const defaultMessages: IHatTip[] = [
+export const defaultPRMessages: IHatTip[] = [
   {
     message: `**$NAME**'s wonderful PR has just been merged. Thank you so much for your contribution! 🚀`,
     emojis: [ '🚀', '🤩' ]
@@ -72,6 +72,102 @@ export const defaultMessages: IHatTip[] = [
     message: `Ahoy, landlubbers! 🦜 Gather 'round and witness the power of code unleashed by the unstoppable **$NAME**. It's P-R time on the CLI for Microsoft 365, where the possibilities are endless! 💪`,
     emojis: ['🦜', '💪']
   },
+  {
+    message: `🎉 Hooray! Let's celebrate the extraordinary contribution of **$NAME** to the CLI for Microsoft 365. ✨🎉 Your code is like magic!`,
+    emojis: ['🎉']
+  },
+  {
+    message: `Behold! **$NAME** has painted the CLI for Microsoft 365 with vibrant colors of innovation. 🎨 Thank you for making our codebase shine! `,
+    emojis: ['🎨']
+  },
+  {
+    message: `Bravo, **$NAME**! Your contribution to the CLI for Microsoft 365 is like a symphony of elegance and functionality. 🎵 Thank you for the harmonious code! 👏`,
+    emojis: ['🎵', '🎻']
+  },
+  {
+    message: `🚀 Blast off! **$NAME** has propelled the CLI for Microsoft 365 to new heights with their groundbreaking contribution. Buckle up for an amazing journey!`,
+    emojis: ['🌌', '🛸']
+  },
+  {
+    message: `Ignite the spark of innovation! 🔥 **$NAME** has set ablaze the CLI for Microsoft 365 with their brilliant contribution. Thank you for lighting up our code! 💡`,
+    emojis: ['💡', '🔥']
+  },
+  {
+    message: `Drumroll, please! 🥁 **$NAME** has earned the gold medal for their exceptional contribution to the CLI for Microsoft 365. Thank you for being a coding champion! 🥇`,
+    emojis: ['🥇', '🥁']
+  },
+  {
+    message: `Step into the spotlight! **$NAME**'s contribution to the CLI for Microsoft 365 deserves a standing ovation. Thank you for being the star of our codebase! ⭐️`,
+    emojis: ['⭐️']
+  },
+  {
+    message: `Together we soar! Thank you, **$NAME**, for your collaborative contribution to the CLI for Microsoft 365. Your teamwork makes our code stronger! 🤝`,
+    emojis: ['🚀', '🤝', '<:pepeHype:1025004228172861500>']
+  },
+  {
+    message: `Bright ideas come to life with **$NAME**! 💡 Thank you for illuminating the CLI for Microsoft 365 with your innovative contribution. Let's keep the ideas flowing!`,
+    emojis: ['💡', '🥳']
+  },
+  {
+    message: `🎊 Congratulations to **$NAME** for their outstanding contribution to the CLI for Microsoft 365! Your code is a work of art. 🖌️`,
+    emojis: ['🎊', '🎨']
+  },
+  {
+    message: `Brace yourselves! **$NAME** has unleashed a tornado of awesomeness upon the CLI for Microsoft 365. Thank you for the whirlwind of innovation! 🌪️`,
+    emojis: ['🌪️', '🔥']
+  },
+  {
+    message: `Buckle up, everyone! **$NAME**'s PR is a rocket ship of brilliance ready to take us to new horizons. Hold on tight for the exciting journey! 🚀✨`,
+    emojis: ['🚀', '✨']
+  },
+  {
+    message: `Attention, all adventurers! Join us in applauding **$NAME** for their daring expedition into the realms of the CLI for Microsoft 365. ⚔️ Thank you for the epic quest!`,
+    emojis: ['⚔️', '🗺️', '🔥']
+  },
+  {
+    message: `Rise and shine, fellow developers! The sun rises on another day of greatness with **$NAME**'s contribution to the CLI for Microsoft 365. Thank you for brightening our codebase! ☀️`,
+    emojis: ['☀️']
+  },
+  {
+    message: `Kudos to **$NAME** for their enchanting contribution to the CLI for Microsoft 365. Your code is like a magical spell that brings our software to life! 🧙✨`,
+    emojis: ['🧙', '✨']
+  },
+  {
+    message: `Calling all heroes! 🦸 **$NAME** has donned their cape and saved the day with their remarkable contribution to the CLI for Microsoft 365. Thank you for being our coding superhero!🔥`,
+    emojis: ['🦸', '🔥', '💪']
+  },
+  {
+    message: `🏆 A standing ovation for **$NAME**! Your extraordinary talent and dedication shine through in your contribution to the CLI for Microsoft 365. Thank you for being an inspiration!`,
+    emojis: ['🏆']
+  },
+  {
+    message: `Rev up your engines! 🏎️ **$NAME** has accelerated the CLI for Microsoft 365 with their high-performance contribution. Thank you for driving us towards success!`,
+    emojis: ['🏎️', '💨']
+  },
+  {
+    message: `Cue the confetti! 🎉 **$NAME**'s PR is a celebration of skill and ingenuity. Thank you for adding joy and excitement to the CLI for Microsoft 365! 🥳`,
+    emojis: ['🎉', '🎊', '🥳']
+  },
+  {
+    message: `Time to satisfy your cravings! 🍪 **$NAME** has returned with a delightful assortment of snacks. Grab a treat and enjoy the deliciousness! 🍿`,
+    emojis: ['🍪', '🍿', '😋']
+  },
+  {
+    message: `Attention cookie enthusiasts! **$NAME** has discovered a hidden treasure trove of delectable cookies. 🍪 Join the cookie quest and indulge in a sweet adventure!`,
+    emojis: ['🍪', '🗺️']
+  },
+  {
+    message: `Snack time alert! **$NAME** has scoured the land to bring you a bounty of mouthwatering treats. 🍫 Treat yourself and savor the flavors of pure happiness! 🍩`,
+    emojis: ['🍩', '🍫']
+  },
+  {
+    message: `Attention, knights of the keyboard! **$NAME** has vanquished the bugs and conquered the CLI for Microsoft 365 with the fury of a thousand unicorns. 🦄 We salute your fearless coding quest! ✨`,
+    emojis: ['🦄', '✨']
+  },
+  {
+    message: `Behold the rise of the wizards of code! 🧙‍♂️ **$NAME** has cast a spell of brilliance upon the CLI for Microsoft 365, harnessing the arcane powers of software development. Your code is truly magical! 🌟`,
+    emojis: ['🧙‍♂️', '🌟', '✨']
+  } 
 ];
 
 export const docPRMessages: IHatTip[] = [
@@ -99,6 +195,83 @@ export const docPRMessages: IHatTip[] = [
     message: `**$NAME** just wrote a new chapter in the CLI for Microsoft 365's success story. ✍️ Thank you for your contribution!`,
     emojis: ['📚', '✍️']
   },
+  {
+    message: `**$NAME** has bookmarked their place in the history of our documentation. 🔖 Thank you for your valuable contribution!`,
+    emojis: ['🔖']
+  },
+  {
+    message: `Mind over matter! 🧠 **$NAME** has expanded the knowledge base of our documentation with their insightful contribution. Thank you for sharing your expertise!`,
+    emojis: ['📚', '🧠']
+  },
+  {
+    message: `Hats off to **$NAME** for their educational documentation contribution. 🎓 You're helping users become masters of the CLI for Microsoft 365!`,
+    emojis: ['🎓', '📚']
+  },
+  {
+    message: `Shine the spotlight on **$NAME** 🔦 for their enlightening contribution to our documentation. Thank you for illuminating the path for our users! ✨`,
+    emojis: ['✨', '🔦', '😍']
+  },
+  {
+    message: `**$NAME** has woven a beautiful tapestry of words in our documentation. 📜 Thank you for adding elegance and clarity to our content!`,
+    emojis: ['📜', '🧵']
+  },
+  {
+    message: `Seeking knowledge? 🔍 Look no further! **$NAME** has enhanced our documentation to provide a treasure trove of information. 🗺️ Thank you for being a guide!`,
+    emojis: ['🔍', '🗺️']
+  },
+  {
+    message: `Blast off to new knowledge frontiers! 🚀 **$NAME** has launched our documentation to new heights with their stellar contribution. Thank you for propelling us forward!`,
+    emojis: ['🚀']
+  },
+  {
+    message: `The pen dances in harmony with **$NAME**'s words, 📝 painting a masterpiece of clarity in our documentation. Thank you for your poetic contribution! 🖌️`,
+    emojis: ['📝']
+  },
+  {
+    message: `**$NAME** has perfectly pieced together the puzzle of our documentation, 🧩 creating a complete picture for our users. Thank you for your meticulous work!`,
+    emojis: ['🧩', '🖼️']
+  },
+  {
+    message: `Unlock the power of knowledge! 🔓 **$NAME** has opened the gates to understanding with their exceptional contribution to our documentation. Thank you for shedding light on the CLI for Microsoft 365! 💡`,
+    emojis: ['🔓']
+  },
+  {
+    message: `In a world of chaos, **$NAME** brings order and clarity to our documentation. 🗂️ Thank you for organizing the knowledge and making it accessible to all!`,
+    emojis: ['📚', '🗂️']
+  },
+  {
+    message: `The documentation realm bows in reverence to **$NAME**. Your words have become the guiding light for users of the CLI for Microsoft 365.`,
+    emojis: ['⭐️', '🌟']
+  },
+  {
+    message: `Step into the realm of enlightenment with **$NAME** as your guide. 🗺️ Their contribution to our documentation is a beacon of wisdom. 🕯️`,
+    emojis: ['🗺️']
+  },
+  {
+    message: `A standing ovation for **$NAME** and their stellar documentation skills! 👏 You've turned complex concepts into easily digestible knowledge.`,
+    emojis: ['👏', '📖']
+  },
+  {
+    message: `**$NAME** has crafted a symphony of words in our documentation, harmonizing information and clarity. 🎵 Thank you for the melodious contribution!`,
+    emojis: ['🎵', '🎶']
+  },
+  {
+    message: `Like a skilled cartographer, **$NAME** has charted the territory of our documentation, providing a clear path for users to follow.`,
+    emojis: ['🗺️', '✏️']
+  },
+  {
+    message: `The ink of knowledge flows through the pen of **$NAME**, 🖋️ creating rivers of wisdom in our documentation. Thank you for the abundant contribution!`,
+    emojis: ['🖋️', '📚']
+  },
+  {
+    message: `Hear ye, hear ye! 📣 The voice of **$NAME** echoes through the halls of our documentation, guiding users towards success. Thank you for your impactful words!`,
+    emojis: ['📣']
+  },
+  {
+    message: `**$NAME** has woven a tapestry of understanding in our documentation, connecting the threads of knowledge. Thank you for the intricate contribution! 🧵`,
+    emojis: ['🧵', '📜']
+  }
+  
 ]
 
 export const bugPRMessages: IHatTip[] = [
@@ -133,5 +306,49 @@ export const bugPRMessages: IHatTip[] = [
   {
     message: `In a galaxy far, far away, **$NAME**'s code has defeated the Dark Sorcerer's bugs in the CLI for Microsoft 365. 🧙‍♂️ May the power of cosmic coding be with you!`,
     emojis: ['🧙‍♂️', '✨']
+  },
+  {
+    message: `Hold on tight! **$NAME** went on a bug-busting adventure and emerged victorious. Thank you for your heroic efforts! 🦸‍♀️`,
+    emojis: ['🦸‍♀️']
+  },
+  {
+    message: `Attention, bug hunters! 🕵️‍♂️ **$NAME** has tracked down and eliminated a notorious bug in the CLI for Microsoft 365. Your expertise is commendable! 🔎`,
+    emojis: ['🕵️‍♂️', '🔎']
+  },
+  {
+    message: `Bug alert! 🚨 **$NAME** rushed to the rescue and fixed the bug in record time. Thank you for your quick action and coding skills!`,
+    emojis: ['🚨', '🔧', '💻']
+  },
+  {
+    message: `Hurray! 🎉 **$NAME** has unleashed their bug-squashing powers and banished a troublesome bug from the CLI for Microsoft 365. We're grateful for your dedication!`,
+    emojis: ['🎉', '💪', '🐞']
+  },
+  {
+    message: `Calling all bug warriors! ⚔️ **$NAME** valiantly fought against a menacing bug and emerged victorious. Thank you for your bravery and expertise!`,
+    emojis: ['⚔️', '🛡️', '🐛']
+  },
+  {
+    message: `Rise and shine, bug conqueror! ☀️ **$NAME** vanquished a stubborn bug in the CLI for Microsoft 365, bringing light to our codebase. Thank you for illuminating our path to excellence!`,
+    emojis: ['☀️', '🐞']
+  },
+  {
+    message: `Hats off to **$NAME** for their bug-crushing prowess! 🎩 Your attention to detail and determination have made our codebase even more robust. Thank you for your invaluable contribution!`,
+    emojis: ['🎩', '🪲']
+  },
+  {
+    message: `In the realm of coding, **$NAME** is a legendary bug slayer. 🐉🛡️ Your skills and expertise have protected our codebase from the jaws of bugs. We salute you!`,
+    emojis: ['🐉', '🛡️']
+  },
+  {
+    message: `Bug exterminated! 🔫💥 **$NAME** took aim and eliminated a pesky bug in the CLI for Microsoft 365. Thank you for your sharpshooting skills and dedication to quality!`,
+    emojis: ['🔫', '💥']
+  },
+  {
+    message: `The CLI for Microsoft 365 is now free from the clutches of a notorious bug, thanks to **$NAME**. 🪲🔓 Your determination and expertise have unlocked a world of possibilities!`,
+    emojis: ['🪲', '🔓']
+  },
+  {
+    message: `Bug slaying season is upon us, and **$NAME** is leading the charge! ⚔️🐞 Your unwavering commitment to excellence has made our codebase stronger. Keep up the fantastic work!`,
+    emojis: ['⚔️', '🔨']
   }
 ]
